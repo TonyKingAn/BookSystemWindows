0e07ab435e865d2cb779cdf036b52cb4cb43d652
AudreyHui <huixj0811@163.com>
2017-12-28 12:08:03 +0800

    update config and MainForm

--- BookSystemWindows/MainForm.cs
+++ BookSystemWindows/MainForm.cs
@@ -1,16 +1,8 @@
-﻿using BookSystemWindows.Models;
+﻿using BookSystemCommon.Models;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Configuration;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.ListView;
-
 namespace BookSystemWindows
 {
     public partial class MainForm : Form
